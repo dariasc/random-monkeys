7f657c74c8e6bbd71fe8253e4691b38f5272d779
Eric <54124297+Nyveon@users.noreply.github.com>
2024-08-31 17:37:53 -0400

    feat: footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,34 @@
 <script>
     import "../app.css";
+
+    const names = ["Davicom", "vi", "Dmitri", "Arias", "Eric"]
+
+    // shuffle names
+    names.sort(() => Math.random() - 0.5);
+
+
+    let nameString = ""
+
+    names.forEach(
+        (name, index) => {
+            if (index === names.length - 1) {
+                nameString += name
+            } else if (index === names.length - 2) {
+                nameString += name + " y "
+            } else {
+                nameString += name + ", "
+            }
+        }
+    )
+
 </script>
 
 <slot />
+
+<footer>
+    <div class="bg-gray-800 text-white p-4 text-center">
+        <p><strong>Monkey Box</strong> por 🐒🐒∞  &nbsp;  ({nameString})</p>
+
+        <em>Segunda Hackathon Random Uchile 31/08/2024</em>
+    </div>
+</footer>
