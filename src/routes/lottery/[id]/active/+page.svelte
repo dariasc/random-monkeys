3d2345b363412329aafb6f3b0430f912bc2c68f8
Eric <54124297+Nyveon@users.noreply.github.com>
2024-08-31 12:46:13 -0400

    feat: tombola view

--- src/routes/lottery/[id]/active/+page.svelte
+++ src/routes/lottery/[id]/active/+page.svelte
@@ -1,6 +1,85 @@
 <script lang="ts">
-	import type { PageData } from './$types'
-	export let data: PageData
+	import type { PageData } from './$types';
+	// import { verify } from '$lib/crypto';
+
+	export let data: PageData;
+
+	// const hashes: string[] = []
+
+	function random256BitsHexadecimal() {
+		//todo: recordar pasar esto ordenadamente
+		return Array.from({ length: 16 }, () => Math.floor(Math.random() * 16).toString(16)).join('');
+	}
+
+	// for testing only!!
+	// for (let i = 0; i < 11; i++) {
+	// 	hashes.push(random256BitsHexadecimal());
+	// }
+
+	// function verifyParticipation(rut: string, salt: string) {
+	// 	return verify(hashes, rut, salt);
+	// }
+
+	const countdownDate = new Date().getTime() + (23 * 60 * 60 * 1000);
+	let countdownTime = "Cargando...";
+
+	const endDate = new Date(countdownDate);
+    const endDateFormatted = endDate.toLocaleString("es-ES", {
+        timeZoneName: "short"
+    });
+
+    const countdownFunction = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = countdownDate - now;
+
+		const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		countdownTime =
+			(days > 0 ? days + "d " : "") +
+			("0" + hours).slice(-2) + "h " +
+			("0" + minutes).slice(-2) + "m " +
+			("0" + seconds).slice(-2) + "s";
+
+        if (distance < 0) {
+            clearInterval(countdownFunction);
+        }
+    }, 1);
 </script>
 
-<h1>{data.id}</h1>
+<main class="bg-gray-100 flex items-center justify-center min-h-screen">
+	<div class="text-center p-6 bg-white shadow-lg max-w-xl rounded-lg w-full">
+		<h1 id="countdown" class="text-5xl font-bold text-blue-600 mb-2">{countdownTime}</h1>
+
+		<p class="text-sm text-gray-500 mb-6">
+			Termina: <span id="raffle-end-date">{endDateFormatted}</span>
+		</p>
+
+
+		<h2 class="text-2xl font-bold text-blue-600 mb-2">Verifica tu participación:</h2>
+
+		<div class="flex items-center justify-center mb-6 space-x-4">
+            <input
+                type="text"
+                placeholder="RUT"
+                class="p-2 border border-gray-300 rounded-md w-1/2"
+            />
+            <input
+                type="text"
+                placeholder="Salt"
+                class="p-2 border border-gray-300 rounded-md w-1/2"
+            />
+        </div>
+
+		<h2 class="text-2xl font-bold text-blue-600 mb-2">Participantes:</h2>
+
+		<div class="grid grid-cols-3 gap-3 w-full">
+			{#each { length: 11 } as _, i}
+				<pre
+					class="flex items-center justify-center p-4 bg-blue-100 rounded text-blue-800 w-full">{random256BitsHexadecimal()}</pre>
+			{/each}
+		</div>
+	</div>
+</main>
