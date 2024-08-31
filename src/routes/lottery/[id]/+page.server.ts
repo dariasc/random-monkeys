33497ca021e6c89c1d52ae88bda110db9f1583e9
svandich <137367720+svandich@users.noreply.github.com>
2024-08-31 17:31:38 -0400

    fix: idk

--- src/routes/lottery/[id]/+page.server.ts
+++ src/routes/lottery/[id]/+page.server.ts
@@ -1,18 +1,55 @@
-import { chooseHashes, generateHashes } from '$lib/crypto';
-import type { PageServerLoad } from './$types'
+import { chooseHashes, generateHashes, randomHex } from '$lib/crypto'
+import { Monkey, MonkeyBox, MonkeySecrets } from '$lib/model'
+import type { UUID } from 'crypto'
+import type { PageServerLoad } from '../$types'
+import { getPulse } from '$lib/random'
 
 export const load: PageServerLoad = async ({ params }) => {
-    const hashes = await generateHashes(["1", "2", "3", "4", "5"])
-    console.log(hashes)
-    const winners = chooseHashes(
-        "dfee0a3fcc04f7acd0e867af6fd43b48a9a9a5074782b20aa70f2fb947f60ec872d6ad6aa109f12d3f92b8fedf56ca394100bd82950ea441e8d9676b186af729",
-        hashes.map((elem) => elem.hash),
-        2
-    ).map((hash) => ({hash}))
+    const monkeyBox = MonkeyBox.get(params.id as UUID)
+    const monkeys = Monkey.getAll(monkeyBox)
+    let hashes;
+    if(monkeyBox.privacy === 'Public'){
+        hashes = monkeys.map((monkey) => {
+            return {
+                id: monkey.value,
+                hash: monkey.hash as string,
+                salt: MonkeySecrets.get(monkey).salt
+            }
+        })
+    } else if(monkeyBox.privacy === 'SemiPrivate'){
+        hashes = monkeys.map((monkey) => {
+            return {
+                id: monkey.value,
+                hash: monkey.hash as string,
+            }
+        })
+    } else if(monkeyBox.privacy === 'Private'){
+        hashes = monkeys.map((monkey) => {
+            return {
+                hash: monkey.hash as string,
+            }
+        })
+    }
 
+    const publishAt = monkeyBox.publishAt*1000
+    let winners: { id?: string, salt?: string, hash: string }[] = []
+    if (Date.now() >= publishAt) {
+        const pulse = await getPulse(monkeyBox.publishAt * 1000)
+        if (pulse) {
+            winners = chooseHashes(
+                pulse,
+                hashes.map((elem) => elem.hash),
+                1
+            ).map((hash) => ({hash}))
+        }
+    }
     return {
-        hashes: hashes.map((obj) => ({hash: obj.hash})),
+        id: params.id,
+        name: "$nombre",
+        isAdmin: Math.random() < 0.5,
+        observer: randomHex(16),
+        hashes: hashes,
         winners: winners,
-        countdownDate: new Date().getTime() + (Number(params.id) * 60 * 60 * 1000),
+        publishAt: monkeyBox.publishAt*1000
     }
 }
