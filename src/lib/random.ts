cebf57aba18d70af4d700103a438beaaa7a421ef
svandich <137367720+svandich@users.noreply.github.com>
2024-08-31 12:47:28 -0400

    feat: add api call

---
+++ src/lib/random.ts
@@ -0,0 +1,28 @@
+export async function getPulse(unix_time: number) {
+  if (unix_time >= new Date().getTime()){
+    throw new Error('Pulse is set in the future');
+  }
+  try {
+    const response = await fetch('https://random.uchile.cl/beacon/2.1-beta/pulse?timeGE='+String(unix_time));
+    const data = await response.json();
+    const pulse: string = data['pulse']['outputValue'];
+    const timestamp = data['pulse']['timeStamp'];
+    const date = new Date(unix_time).toISOString();
+
+    const prefix1 = timestamp.substring(0, 17);
+    const prefix2 = date.substring(0, 17);
+
+    if(prefix1 === prefix2){
+        if (typeof pulse === 'string' && pulse != ''){
+            return pulse;
+        } else {
+            console.log('pulse not found');
+        }
+    } else {
+        console.log('pulse time does not match asked time');
+    }
+
+  } catch (err) {
+    console.log(err);
+  }
+};
