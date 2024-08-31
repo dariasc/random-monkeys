97de6da828a2b33b487b895cd85f73e1ad797fa4
Diego Arias <dariascauas@gmail.com>
2024-08-31 11:14:11 -0400

    feat: implement crypto fns

---
+++ src/lib/crypto.ts
@@ -0,0 +1,29 @@
+import {
+  createHash,
+  randomBytes
+} from 'crypto';
+
+function generateSalt() {
+  const salt = randomBytes(64)
+  return salt.toString('hex')
+}
+
+function generateHashes(ids: string[]) {
+  return ids.map((id) => {
+    const salt = generateSalt()
+    const hash = hashUser(id, salt)
+    return {
+      id,
+      salt,
+      hash
+    }
+  })
+}
+
+function hashUser(id: string, salt: string) {
+  const hash = createHash('sha256')
+  hash.update(id + salt)
+  return hash.digest('hex')
+}
+
+export { hashUser, generateHashes }
